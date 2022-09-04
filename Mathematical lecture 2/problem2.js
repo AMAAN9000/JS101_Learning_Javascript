8fd57a0a293fe54713ce5fc8fce8d3cd4f4b7e78
AMAAN9000 <amaanmohd90@gmail.com>
2022-09-04 21:14:29 +0530

    adding solution of lecture 2

---
+++ Mathematical lecture 2/problem2.js
@@ -0,0 +1,18 @@
+const { get } = require("http");
+
+let one = 1;
+let two = 2;
+let three = 3;
+let four = 4;
+let five = 5;
+
+// after doubling the value we get
+
+let new_one = 1*2;
+let new_two = 2*2;
+let new_three = 3*2;
+let new_four = 4*2;
+let new_five = 5*2;
+
+console.log(new_one +new_two+new_three +new_four+new_five);
+
---
+++ Mathematical lecture 2/problem3.js
@@ -0,0 +1,23 @@
+
+
+let one = 5;
+let two = 4;
+let three = 9;
+let four = 8;
+let five = 2;
+let six = 20;
+
+
+// new values A/Q 
+
+let new_one = 5;
+let new_two =4*2;
+let new_three = 9*3;
+  let new_four =8*3;
+  let new_five =2*2;
+  let new_six = 20*2;
+
+console.log(new_one +new_two+new_three +new_four+new_five+new_six);
+
+
+
