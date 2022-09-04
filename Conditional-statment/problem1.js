e8ee4485d0ed32cf45a2be8f4d4522540d2faa4f
AMAAN9000 <amaanmohd90@gmail.com>
2022-09-04 21:32:33 +0530

    Adding codes of Conditonal Statement

---
+++ Conditional-statment/problem1.js
@@ -0,0 +1,7 @@
+let n= 15;
+if(n%3==0){
+  console.log("Multiple of three");
+}else{
+  console.log("not a multiple of three");
+}
+
---
+++ Conditional-statment/problem4.js
@@ -0,0 +1,27 @@
+let reg_gmail = "xyz@gmail.com";
+let reg_password = "456789";
+
+let put_gmail = "xybprobz@gmail.com";
+let put_password = "4567899";
+
+if (reg_gmail==put_gmail){
+  
+
+
+ if (reg_password==put_password){
+
+  
+  console.log("Login succesfully");
+}
+else {
+  console.log("Wrong password");
+  
+}
+
+} else {
+  console.log("Wrong credencials");
+}
+
+
+
+
